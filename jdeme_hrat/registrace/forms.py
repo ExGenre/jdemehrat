7c061123ee836ce4b470b0a9bb3fbedf903df809
ExGenre <ex.genre@hotmail.com>
2024-01-13 00:36:50 +0100

    update

--- jdeme_hrat/registrace/forms.py
+++ jdeme_hrat/registrace/forms.py
@@ -1,30 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+from .models import Event
+
+
 
 class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+class LoginForm(forms.Form):
     email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
-
-class EmailLoginForm(AuthenticationForm):
-    username = forms.EmailField(
-        label="Email",
-        widget=forms.EmailInput(attrs={'autofocus': True}),
-        required=True,
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Upravíme pole username, aby mělo stejný label jako email
-        self.fields['username'].label = 'Email'
-
-    def clean(self):
-        # Překopírovat hodnotu z pole email do pole username, pokud existuje
-        if 'username' in self.cleaned_data:
-            self.cleaned_data['username'] = self.cleaned_data['username']
-        return super().clean()
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['nazev', 'popis', 'hra', 'misto', 'datum_konani']
--- jdeme_hrat/registrace/urls.py
+++ jdeme_hrat/registrace/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import register, profile, profile_list
+from django.urls import path, include
+from .views import register, profile, profile_list, events, create_event, EventViewSet, user_login
 from django.contrib.auth.views import LogoutView
-from .views import user_login
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -9,4 +12,8 @@ urlpatterns = [
     path('profile_list/', profile_list, name='profile_list'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user_login/', user_login, name='user_login'),
+    path('login/', user_login, name='login'),
+    path('events/', events, name='events'),
+    path('create_event/', create_event, name='create_event'),
+    path('api/', include(router.urls))
 ]
